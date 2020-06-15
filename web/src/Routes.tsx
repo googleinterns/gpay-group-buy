0035a056a7fcc6060096a4a6498456de18410a83
Caryn Heng <carynheng@google.com>
2020-06-15 19:26:46 +0800

    Enable absolute import

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,11 +1,11 @@
 import React, {lazy, Suspense} from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Loading from './components/common/Loading';
+import Loading from 'components/common/Loading';
 
 // Customer Pages
-const CustomerExplorePage = lazy(() => import('./components/customer/explore'));
+const CustomerExplorePage = lazy(() => import('components/customer/explore'));
 // Merchant Pages
-const MerchantLandingPage = lazy(() => import('./components/merchant/landing'));
+const MerchantLandingPage = lazy(() => import('components/merchant/landing'));
 
 const Routes: React.FC = () => (
   <Suspense fallback={<Loading />}>
