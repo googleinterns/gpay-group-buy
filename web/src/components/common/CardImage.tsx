cae62abf90f7bfb818d9ddc933475e42d04d1ddc
Caryn Heng <carynheng@google.com>
2020-06-18 13:17:02 +0800

    Add ListingCard component

---
+++ web/src/components/common/CardImage.tsx
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+import {Image} from 'interfaces';
+
+const MIN_IMG_HEIGHT = '120px';
+
+interface CardContainerProps {
+  width?: string;
+  height?: string;
+}
+
+const CardContainer = styled.div`
+  width: ${({width}: CardContainerProps) => width || '100%'};
+  min-height: DEFAULT_IMG_HEIGHT;
+  height: ${({height}: CardContainerProps) => height || MIN_IMG_HEIGHT};
+  overflow: hidden;
+`;
+
+const StyledCardImage = styled.img`
+  width: inherit;
+  height: inherit;
+  object-fit: cover;
+  background-color: var(--light-gray);
+`;
+
+interface CardImageProps {
+  img: Image;
+  width?: string;
+  height?: string;
+}
+
+const CardImage: React.FC<CardImageProps> = ({img, width, height}) => (
+  <CardContainer width={width} height={height}>
+    <StyledCardImage src={img.url} alt={img.alt || 'Card Image'} />
+  </CardContainer>
+);
+
+export default CardImage;
---
+++ web/src/components/common/ListingCard.tsx
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+import Card from 'components/common/Card';
+import ListingPrice from 'components/common/ListingPrice';
+
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 0.8em;
+
+  & > * {
+    margin: 0;
+  }
+`;
+
+const Date = styled.span`
+  display: flex;
+  justify-content: flex-end;
+
+  font-size: 0.8rem;
+  color: var(--red);
+`;
+
+const ListingName = styled.span`
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  color: var(--gray);
+`;
+
+interface ListingCardProps {
+  listingName: string;
+  price: number;
+  oldPrice: number;
+  endDate: string;
+  imgUrl: string;
+  horizontal?: boolean;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  listingName,
+  price,
+  oldPrice,
+  imgUrl,
+  endDate,
+  horizontal,
+  children,
+}) => (
+  <Card
+    img={{
+      url: imgUrl,
+      alt: `Image of ${listingName}`,
+    }}
+    imgLeft={horizontal}
+  >
+    <CardContent>
+      <Date>{endDate}</Date>
+      <ListingName>{listingName}</ListingName>
+      <ListingPrice price={price} oldPrice={oldPrice} />
+      {children}
+    </CardContent>
+  </Card>
+);
+
+export default ListingCard;
