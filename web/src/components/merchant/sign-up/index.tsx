7b0eb428a1992143d4a714f71e158f19e9490763
Karen Frilya Celine <karenfrilya@google.com>
2020-06-19 16:04:36 +0800

    Vertically centralise SignUpCard in SignUpPage

--- web/src/components/merchant/sign-up/index.tsx
+++ web/src/components/merchant/sign-up/index.tsx
@@ -21,8 +21,8 @@ import Container from 'muicss/lib/react/container';
 import styled from 'styled-components';
 
 const PageContainer = styled(Container)`
-  max-height: 800px;
-  height: 100vh;
+  min-height: 100vh;
+  height: 100%;
   width: 100%;
 
   display: flex;
