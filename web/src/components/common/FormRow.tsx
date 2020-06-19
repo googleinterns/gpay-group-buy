9407acb421a48553adf80baa15c3cf490eac4eff
Karen Frilya Celine <karenfrilya@google.com>
2020-06-19 15:47:04 +0800

    Strip input tag style in index.css file instead of FormRox.tsx

--- web/src/components/common/FormRow.tsx
+++ web/src/components/common/FormRow.tsx
@@ -42,10 +42,6 @@ const Input = styled.input`
   border-radius: 15px;
   border: none;
   box-shadow: 4px 2px 4px rgba(0, 0, 0, 0.25);
-
-  &:focus {
-    outline: none;
-  }
 `;
 
 interface FormRowProps {
