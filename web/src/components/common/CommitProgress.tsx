b94176fa3f0ae62939d0f8db94371994a7a28d60
Caryn Heng <carynheng@google.com>
2020-06-19 16:05:46 +0800

    Add commit progress bar (#50)
    * Add commit progress bar
    
    * Edit according to comments
    
    * Add progress bar to design samples
    
    * Add hack to fix progressbar height

---
+++ web/src/components/common/CommitProgress.tsx
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import {Line} from 'rc-progress';
+import styled from 'styled-components';
+
+const DEFAULT_HEIGHT = 10;
+
+type CommitTextPos = 'none' | 'top' | 'bottom';
+
+interface CommitProgressContainerProps {
+  textPos?: CommitTextPos;
+}
+
+const CommitProgressContainer = styled.div`
+  display: flex;
+  flex-direction: ${({textPos}: CommitProgressContainerProps) => {
+    switch (textPos) {
+      case 'top':
+        return 'column';
+      case 'bottom':
+        return 'column-reverse';
+      default:
+        return 'column';
+    }
+  }};
+  text-align: right;
+  font-size: 0.8em;
+`;
+
+const StyledLine = styled(Line)`
+  /* Hack to force Line to be a certain height without being scaled,
+   because it is an svg under the hood. */
+  min-height: ${DEFAULT_HEIGHT}px;
+  max-height: ${DEFAULT_HEIGHT}px;
+  border-radius: ${DEFAULT_HEIGHT / 2}px;
+`;
+
+const CommitCount = styled.span`
+  font-weight: bold;
+`;
+
+interface CommitProgressProps {
+  numCommits: number;
+  minCommits: number;
+  textPos?: CommitTextPos;
+  thicker?: boolean;
+}
+
+/**
+ * Returns a progress bar indicating the number of Commiters aginst
+ * the min number of commits needed.
+ */
+const CommitProgress: React.FC<CommitProgressProps> = ({
+  numCommits,
+  minCommits,
+  textPos = 'bottom',
+}) => {
+  const percent = Math.min(100, (numCommits / minCommits) * 100);
+
+  return (
+    <CommitProgressContainer textPos={textPos}>
+      {textPos !== 'none' && (
+        <div>
+          <span>Commiters: </span>
+          <CommitCount>
+            {numCommits}/{minCommits}
+          </CommitCount>
+        </div>
+      )}
+      <StyledLine
+        percent={percent}
+        strokeColor="var(--green)"
+        trailColor="var(--pale-green)"
+        strokeLinecap="square"
+      />
+    </CommitProgressContainer>
+  );
+};
+
+export default CommitProgress;
