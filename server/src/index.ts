d28e04702a10830afd10288a16dc2e685086c23f
Caryn Heng <carynheng@google.com>
2020-06-22 20:26:29 +0800

    Add stop handler on term signals

--- server/src/index.ts
+++ server/src/index.ts
@@ -66,5 +66,10 @@ app.use(
 );
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-console.log(`Listening on port ${port}`);
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+const stopHandler = () => process.exit(1);
+
+process.on('SIGTERM', stopHandler);
+process.on('SIGINT', stopHandler);
+process.on('SIGHUP', stopHandler);
