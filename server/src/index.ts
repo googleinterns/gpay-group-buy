944d0cf9005e9f9b02266f7a6e65e59c06b53d26
Caryn Heng <carynheng@google.com>
2020-06-22 20:37:04 +0800

    Disable lint rule for line

--- server/src/index.ts
+++ server/src/index.ts
@@ -68,7 +68,7 @@ app.use(
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const stopHandler = () => process.exit(1);
+const stopHandler = () => process.exit(1); // eslint-disable-line no-process-exit
 
 process.on('SIGTERM', stopHandler);
 process.on('SIGINT', stopHandler);
