5e54b5f00bc2f288d3e84fc3254bb9126d8e434c
Caryn Heng <carynheng@google.com>
2020-06-24 19:07:01 +0800

    Fix lint

--- web/src/components/design-samples/index.tsx
+++ web/src/components/design-samples/index.tsx
@@ -20,9 +20,9 @@ import Card from 'components/common/Card';
 import CommitProgress from 'components/common/CommitProgress';
 import ListingCard from 'components/common/ListingCard';
 import StrippedCol from 'components/common/StrippedCol';
+import {formatRFC3339} from 'date-fns';
 import {Money} from 'interfaces';
 import Container from 'muicss/lib/react/container';
-import { formatRFC3339 } from 'date-fns';
 
 const SAMPLE_IMG_URL = 'https://picsum.photos/seed/picsum/200/300';
 const SAMPLE_END_DATE = formatRFC3339(new Date('2020-07-10T23:59:59Z'));
