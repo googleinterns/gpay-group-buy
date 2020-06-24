090417bed11e4b5ba1ea3cad264ab3ca600d1fde
Caryn Heng <carynheng@google.com>
2020-06-24 14:52:54 +0800

    Add fetch customer from endpoint

--- web/src/interfaces.ts
+++ web/src/interfaces.ts
@@ -2,3 +2,11 @@ export interface Image {
   url: string;
   alt?: string;
 }
+
+export interface Customer {
+  id: number;
+  gpayId: string;
+  contactNumber: string;
+  address: string;
+  numCommits: number;
+}
