3cbdb69cc778ff70ae2271965274cb6cdcc572ba
Karen Frilya Celine <karenfrilya@google.com>
2020-06-30 01:24:36 +0800

    Add merchant interfaces

--- web/src/interfaces.ts
+++ web/src/interfaces.ts
@@ -10,3 +10,22 @@ export interface Customer {
   address: string;
   numCommits: number;
 }
+
+/**
+ * MerchantPayload Interface that contains the fields of the payload that
+ * would be sent to the server to add the merchant to the database.
+ */
+export interface MerchantPayload {
+  name: string;
+  email: string;
+  vpa: string;
+}
+
+/**
+ * MerchantResponse Interface that contains the fields of the Response that
+ * client side would receive.
+ */
+export interface MerchantResponse extends MerchantPayload {
+  id: number;
+  firebaseUid: string;
+}
