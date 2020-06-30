519e4ce756c461445f4692fa9fddd0f2d843a87f
Karen Frilya Celine <karenfrilya@google.com>
2020-06-30 13:36:00 +0800

    Add dotenv library to read .env file using node

--- server/src/api/merchants.ts
+++ server/src/api/merchants.ts
@@ -18,11 +18,14 @@
  * @fileoverview Handles routing of /merchants endpoints.
  */
 
+import dotenv from 'dotenv';
 import {Router, Request, Response, NextFunction} from 'express';
 
 import {MerchantPayload} from '../interfaces';
 import merchantAuth from '../middleware/merchant-auth';
 import {merchantService} from '../services';
+
+dotenv.config();
 
 const merchantRouter: Router = Router();
 
