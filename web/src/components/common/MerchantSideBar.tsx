c8402c769c67448ce4908b56a4eb8c12f114a8d6
Karen Frilya Celine <karenfrilya@google.com>
2020-07-09 01:19:43 +0800

    Add MerchantSideBar component

--- web/src/components/common/MerchantSideBar.tsx
+++ web/src/components/common/MerchantSideBar.tsx
@@ -16,28 +16,93 @@
 
 import React from 'react';
 
+import Button from 'components/common/Button';
+import CentralisedContainer from 'components/common/CentralisedContainer';
+import {MerchantResponse} from 'interfaces';
+import MuiRow from 'muicss/lib/react/row';
 import styled from 'styled-components';
 
 import {ReactComponent as CreditCard} from 'assets/credit-card.svg';
 import {ReactComponent as Mail} from 'assets/mail.svg';
 import {ReactComponent as Shop} from 'assets/shop.svg';
 
+const SideBarContainer = styled(CentralisedContainer)`
+  min-height: 100vh;
+  height: 100%;
+  width: 20%;
+
+  box-shadow: 5px 0px 10px var(--light-gray);
+  margin: 0;
+
+  justify-content: space-between;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+`;
+
+const Row = styled(MuiRow)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const IconContainer = styled(CentralisedContainer)`
+  width: 10%;
+  margin-right: 3%;
+`;
+
+const TextContainer = styled.div`
+  width: 100%;
+  justify-content: left;
+  overflow-wrap: anywhere;
+`;
+
+const StyledButton = styled(Button)`
+  font-size: 0.85em;
+`;
+
 /**
  * This is a side bar for merchant pages. It shows merchant's profile and buttons
  * to view merchant's ongoing and past listings.
  */
-const GroupBuyMerchantHeader: React.FC = () => {
+const MerchantSideBar: React.FC<MerchantResponse> = ({name, email, vpa}) => {
   return (
-    <CentralisedContainer>
-      <Shop />
-      <div></ div>
-      <Header>
-        Group Buy
-        <br />
-        <b>Merchant</b>
-      </Header>
-    </CentralisedContainer>
+    <SideBarContainer>
+      <CentralisedContainer>
+        <Shop />
+        <Header>{name}</Header>
+        <DetailsContainer>
+          <Row>
+            <IconContainer>
+              <Mail />
+            </IconContainer>
+            <TextContainer>karenfrilya97@gmail.com</TextContainer>
+          </Row>
+          <Row>
+            <IconContainer>
+              <CreditCard />
+            </IconContainer>
+            <TextContainer>{vpa}</TextContainer>
+          </Row>
+        </DetailsContainer>
+      </CentralisedContainer>
+      <CentralisedContainer>
+        <StyledButton>View Ongoing Listings</StyledButton>
+        <StyledButton>View Past Listings</StyledButton>
+      </CentralisedContainer>
+    </SideBarContainer>
   );
 };
 
-export default GroupBuyMerchantHeader;
+export default MerchantSideBar;
