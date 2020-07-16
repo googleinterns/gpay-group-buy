38427f2c2475845c161c9c221f11a02fc9d431c1
Caryn Heng <carynheng@google.com>
2020-07-16 13:47:36 +0800

    Add check for listingStatus

--- server/src/services/commits.ts
+++ server/src/services/commits.ts
@@ -74,21 +74,18 @@ const addCommit = async (
   commitData: CommitRequest
 ): Promise<CommitResponse> => {
   // Make sure that listing and customer exists
-  await listingStorage.getListing(commitData.listingId);
-  await customerStorage.getCustomer(commitData.customerId);
+  const listing = await listingStorage.getListing(commitData.listingId);
+  const customer = await customerStorage.getCustomer(commitData.customerId);
+
+  // Check that listing is ongoing
+  if (listing.listingStatus !== 'ongoing') {
+    throw new Error(
+      `Not allowed to commit to ${listing.listingStatus} listing.`
+    );
+  }
 
   // Check that customer has not reached max num of ongoing commits
-  const customerCommits = await commitStorage.getAllCommits([
-    {
-      property: 'customerId',
-      value: commitData.customerId,
-    },
-    {
-      property: 'commitStatus',
-      value: 'ongoing',
-    },
-  ]);
-  if (customerCommits.length >= MAX_NUM_COMMITS) {
+  if (customer.numOngoingCommits >= MAX_NUM_COMMITS) {
     throw new Error(
       `Customer has reached max number of ${MAX_NUM_COMMITS} ongoing commits.`
     );
