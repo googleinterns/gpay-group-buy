79bc13a947909097af2f0854dd5291504cf2304e
Caryn Heng <carynheng@google.com>
2020-07-17 13:04:04 +0800

    Add check for listingStatus on add commit endpoint (#131)
    * Add check for listingStatus
    
    * Address comments

--- server/src/services/commits.ts
+++ server/src/services/commits.ts
@@ -73,22 +73,19 @@ const getAllCommits = async (
 const addCommit = async (
   commitData: CommitRequest
 ): Promise<CommitResponse> => {
-  // Make sure that listing and customer exists
-  await listingStorage.getListing(commitData.listingId);
-  await customerStorage.getCustomer(commitData.customerId);
+  // Check that listing and customer exists
+  const listing = await listingStorage.getListing(commitData.listingId);
+  const customer = await customerStorage.getCustomer(commitData.customerId);
+
+  // Check that listing is ongoing
+  if (listing.listingStatus !== 'ongoing') {
+    throw new Error(
+      `Not allowed to commit to ${listing.listingStatus} listing.`
+    );
+  }
 
   // Check that customer has not reached max num of ongoing commits
-  const customerCommits = await commitStorage.getAllCommits([
-    {
-      property: 'customerId',
-      value: commitData.customerId,
-    },
-    {
-      property: 'commitStatus',
-      value: 'ongoing',
-    },
-  ]);
-  if (customerCommits.length >= MAX_NUM_COMMITS) {
+  if (customer.numOngoingCommits >= MAX_NUM_COMMITS) {
     throw new Error(
       `Customer has reached max number of ${MAX_NUM_COMMITS} ongoing commits.`
     );
