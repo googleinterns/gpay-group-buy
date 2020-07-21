0f2daf17feb3472ae5674087113c9cde53e93b56
Caryn Heng <carynheng@google.com>
2020-07-21 13:12:38 +0800

    Add merchant details to listing details page (#148)
    * Add merchant details section to listing page
    
    * Fix docstrings
    
    * Use Link to link to merchant profile
    
    * Fix lint
    
    * Fix css lint
    
    * Add todo
    
    * Update docstring

---
+++ web/src/components/customer/listing-details/MerchantDetails.tsx
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {useState, useEffect} from 'react';
+
+import {getMerchantWithId} from 'api';
+import {MerchantResponse} from 'interfaces';
+import Container from 'muicss/lib/react/container';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+import {ReactComponent as Shop} from 'assets/merchant/shop.svg';
+
+const MerchantDetailsContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  padding: 0.5rem 0;
+
+  & > * {
+    padding: 0.5rem;
+  }
+`;
+
+const MerchantIcon = styled(Shop)`
+  max-height: 55px;
+  max-width: 55px;
+`;
+
+const ContentContainer = styled.div`
+  & > * {
+    padding: 6px 0;
+  }
+`;
+
+const MerchantName = styled.div`
+  font-size: 1rem;
+  color: var(--dark-gray);
+`;
+
+const LinkButton = styled(Link)`
+  color: var(--green);
+  font-weight: bold;
+  text-transform: capitalize;
+`;
+
+interface MerchantDetailsProps {
+  merchantId: number;
+}
+
+/**
+ * MerchantDetails that contains basic merchant details,
+ * as well as a link to the merchant profile page.
+ */
+const MerchantDetails: React.FC<MerchantDetailsProps> = ({merchantId}) => {
+  const [merchant, setMerchant] = useState<MerchantResponse>();
+
+  useEffect(() => {
+    const fetchMerchant = async () => {
+      const merchant = await getMerchantWithId(merchantId);
+      setMerchant(merchant);
+    };
+    fetchMerchant();
+  }, [merchantId]);
+
+  // TODO: Update Link to redirect to merchant profile when it is up.
+  return (
+    <>
+      {merchant && (
+        <MerchantDetailsContainer>
+          <MerchantIcon />
+          <ContentContainer>
+            <MerchantName>{merchant.name}</MerchantName>
+            <LinkButton to="/">View Merchant Profile</LinkButton>
+          </ContentContainer>
+        </MerchantDetailsContainer>
+      )}
+    </>
+  );
+};
+
+export default MerchantDetails;
