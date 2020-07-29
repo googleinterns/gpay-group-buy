d3b400fe382375b18bde52f2eb02b911d9ecfd46
Caryn Heng <carynheng@google.com>
2020-07-29 21:33:48 +0800

    Add loading to Explore page

--- web/src/components/customer/explore/index.tsx
+++ web/src/components/customer/explore/index.tsx
@@ -19,6 +19,7 @@ import React, {useEffect, useState} from 'react';
 import {getAllListings} from 'api';
 import CommitsBadge from 'components/common/CommitsBadge';
 import ListingCollection from 'components/common/ListingCollection';
+import Loading from 'components/common/Loading';
 import {useCustomerContext} from 'components/customer/contexts/CustomerContext';
 import {Listing} from 'interfaces';
 import Button from 'muicss/lib/react/button';
@@ -42,12 +43,14 @@ const CustomerExplorePage: React.FC = () => {
 
   const handleGetIdentity = async () => getCustomerWithLogin();
 
+  const [isListingsLoading, setIsListingsLoading] = useState(true);
   const [listings, setListings] = useState<Listing[]>([]);
 
   useEffect(() => {
     const fetchListings = async () => {
       const listings = await getAllListings();
       setListings(listings);
+      setIsListingsLoading(false);
     };
     fetchListings();
   }, []);
@@ -63,7 +66,11 @@ const CustomerExplorePage: React.FC = () => {
         </Button>
       </Link>
       <h1>Explore</h1>
-      <ListingCollection listings={listings} />
+      {isListingsLoading ? (
+        <Loading />
+      ) : (
+        <ListingCollection listings={listings} />
+      )}
       <Button color="primary" onClick={handleGetIdentity}>
         Get Identity
       </Button>
